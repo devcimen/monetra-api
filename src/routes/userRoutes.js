96dc6e52525be841f21f38187c7b1535f453b59f
swt-ec <emre.cimen@swissteach.com>
2025-01-21 10:55:16 +0100

    Finished user routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,13 @@
 // Endpoints for user routes
 const express = require('express');
-const { registerUser, loginUser } = require('../controllers/userControllers');
+const { registerUser, loginUser, getUserProfile, updateUserProfile } = require('../controllers/userController');
+const { protect } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
 // Routes
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
 
 module.exports = router;
